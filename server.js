e13dc3c3ebd64e4fe7b9fa25cb909283c7ca4c36
Fikri <muhammadfikrie1108@gmail.com>
2026-09-08 11:20:28 +0700

    Update : Fix embedded image optimization

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const { exec, execFile, execSync } = require('child_process');
+const { exec, execFile, execFileSync, execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
 const JSZip = require('jszip');
